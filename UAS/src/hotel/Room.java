8b5c675f762f9b0fdd0dd830f45c06661577f55f
Irhan Hisyam <108678365+irhanhisyamdwinugroho@users.noreply.github.com>
2023-01-05 13:40:47 +0800

    Create Room.java

---
+++ UAS/src/hotel/Room.java
@@ -0,0 +1,33 @@
+package com.hotel;
+
+
+// Kelas Room untuk menyimpan data kamar
+class Room {
+    private int number;
+    private String type;
+    private int price;
+
+    public Room(int number, String type, int price) {
+        this.number = number;
+        this.type = type;
+        this.price = price;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
